ce5aa371d60e18971fa3eb072e17196bf3900c16
ahmedibrahim240 <Ahmedibrahim1310@icloud.com>
2025-10-01 15:44:49 +0200

    ..

--- lib/view/about/ui/widget/hero_text.dart
+++ lib/view/about/ui/widget/hero_text.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:my_portfolio/core/helper/app_size.dart';
 import 'package:my_portfolio/core/helper/extensions.dart';
 import 'package:my_portfolio/core/widget/app_text.dart';
 import 'package:my_portfolio/view/about/models/about_me_models.dart';
+import 'package:pretty_animated_text/pretty_animated_text.dart';
 // import 'package:seo_renderer/seo_renderer.dart';
 
 class HeroText extends StatelessWidget {
@@ -25,52 +27,76 @@ class HeroText extends StatelessWidget {
                 ? CrossAxisAlignment.start
                 : CrossAxisAlignment.center,
             children: [
-              AppText(
-                kAboutMe.fullName,
-                textAlign: context.isDesktop ? TextAlign.left : TextAlign.center,
-                style: context.textStyle.titleLgBold.copyWith(
-                  color: context.theme.colorScheme.onBackground,
-                ),
-                // textRendererStyle: TextRendererStyle.header1,
-              ),
-              Gap(AppSize.xxl),
+              const BuildNameAndJob(),
+              Gap(AppSize.xl),
               OutlinedButton(onPressed: () {}, child: const Text('Download CV')),
             ],
           ),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              AppText(
-                kAboutMe.fullName,
-                textAlign: context.isDesktop ? TextAlign.left : TextAlign.center,
-                style: context.textStyle.titleLgBold.copyWith(
-                  color: context.theme.colorScheme.onBackground,
-                ),
-                // textRendererStyle: TextRendererStyle.header1,
-              ),
+              const BuildNameAndJob(),
               Gap(AppSize.xxl),
               OutlinedButton(onPressed: () {}, child: const Text('Download CV')),
             ],
           ),
         ),
         Gap(AppSize.smallSized),
+        OffsetText(
+          text: kAboutMe.summary,
+          duration: const Duration(milliseconds: 800),
+          slideType: SlideAnimationType.alternateLR,
+          textAlignment: context.isDesktopOrTablet
+              ? TextAlignment.start
+              : TextAlignment.center,
+          textStyle: context.textStyle.titleMdMedium.copyWith(
+            color: context.theme.colorScheme.onSurface,
+            height: 2,
+          ),
+        ),
+        // DefaultTextStyle(
+        //   textAlign: context.isDesktopOrTablet ? TextAlign.left : TextAlign.center,
+        //   style: context.textStyle.titleMdMedium.copyWith(
+        //     color: context.theme.colorScheme.onSurface,
+        //     height: 2,
+        //   ),
+        //   child: AnimatedTextKit(
+        //     isRepeatingAnimation: false,
+        //     animatedTexts: [TyperAnimatedText(kAboutMe.summary)],
+        //   ),
+        // ),
+      ],
+    );
+  }
+}
+
+class BuildNameAndJob extends StatelessWidget {
+  const BuildNameAndJob({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
         AppText(
-          kAboutMe.title,
+          kAboutMe.fullName,
           textAlign: context.isDesktop ? TextAlign.left : TextAlign.center,
-          style: context.textStyle.titleMdMedium.copyWith(
+          style: context.textStyle.titleLgBold.copyWith(
             color: context.theme.colorScheme.onBackground,
           ),
           // textRendererStyle: TextRendererStyle.header1,
         ),
-        Gap(AppSize.xxl),
-        AppText(
-          kAboutMe.summary,
-          textAlign: context.isDesktopOrTablet ? TextAlign.left : TextAlign.center,
-          style: context.textStyle.bodyMdMedium.copyWith(
-            color: context.theme.colorScheme.onSurface,
-            height: 2,
+        Gap(AppSize.smallSized),
+        DefaultTextStyle(
+          textAlign: context.isDesktop ? TextAlign.left : TextAlign.center,
+          style: context.textStyle.titleMdMedium.copyWith(
+            color: context.theme.colorScheme.onBackground,
           ),
-          // textRendererStyle: TextRendererStyle.header1,
+          child: AnimatedTextKit(
+            repeatForever: true,
+
+            animatedTexts: [TyperAnimatedText(kAboutMe.title)],
+          ),
         ),
       ],
     );
