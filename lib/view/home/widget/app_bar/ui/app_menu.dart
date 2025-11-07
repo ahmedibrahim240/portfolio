b6210d8fb4071f15ed660a0e909c2c7b1a37f051
ahmedibrahim240 <Ahmedibrahim1310@icloud.com>
2025-11-07 15:27:37 +0200

    ..

--- lib/view/home/widget/app_bar/ui/app_menu.dart
+++ lib/view/home/widget/app_bar/ui/app_menu.dart
@@ -11,7 +11,7 @@ import 'package:my_portfolio/view/home/widget/app_bar/logic/cubit/drawer_menu_st
 import 'package:my_portfolio/view/home/widget/app_bar/logic/scroll/scroll_cubit_cubit.dart';
 import 'package:my_portfolio/view/home/widget/app_bar/logic/scroll/scroll_cubit_state.dart';
 
-class MenuItem extends StatelessWidget {
+class MenuItem extends StatefulWidget {
   final String title;
   final bool isSelected;
   final VoidCallback onTap;
@@ -23,23 +23,94 @@ class MenuItem extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppSize.medSized,
-          vertical: AppSize.xsSized,
-        ),
-        child: Text(
-          title,
+  State<MenuItem> createState() => _MenuItemState();
+}
 
-          style: SmallTextStyles().titleSmBold.copyWith(
-            color: isSelected
-                ? context.theme.colorScheme.onBackground
-                : context.theme.colorScheme.onSurface,
-            fontWeight: isSelected ? FontWeight.w700 : FontWeight.w200,
-          ),
+class _MenuItemState extends State<MenuItem> with SingleTickerProviderStateMixin {
+  bool _isHovered = false;
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.05,
+    ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+  }
+
+  @override
+  void didUpdateWidget(MenuItem oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isSelected) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Color defaultColor = widget.isSelected
+        ? context.theme.colorScheme.onBackground
+        : context.theme.colorScheme.onSurface;
+    final Color hoverColor = context.theme.primaryColor;
+
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() => _isHovered = true);
+        _animationController.forward();
+      },
+      onExit: (_) {
+        setState(() => _isHovered = false);
+        if (!widget.isSelected) {
+          _animationController.reverse();
+        }
+      },
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: _scaleAnimation.value,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppSize.medSized,
+                  vertical: AppSize.xsSized,
+                ),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: _isHovered || widget.isSelected
+                          ? hoverColor
+                          : Colors.transparent,
+                      width: 2,
+                    ),
+                  ),
+                ),
+                child: Text(
+                  widget.title,
+                  style: SmallTextStyles().titleSmBold.copyWith(
+                    color: _isHovered ? hoverColor : defaultColor,
+                    fontWeight: widget.isSelected ? FontWeight.w700 : FontWeight.w400,
+                    letterSpacing: _isHovered ? 0.5 : 0.0,
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
@@ -114,32 +185,51 @@ class _DrawerMenuState extends State<DrawerMenu> with SingleTickerProviderStateM
 }
 
 class MobileMenu extends StatelessWidget {
-  final bool isFromFooter;
-  const MobileMenu({super.key, this.isFromFooter = false});
+  const MobileMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<ScrollCubitCubit, ScrollCubitState>(
       builder: (context, state) {
         final ScrollCubitCubit scrollCubit = context.read<ScrollCubitCubit>();
-        final drawerCubit = context.read<DrawerMenuCubit>();
         return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: AppMenuModel.menuList
-              .map(
-                (e) => MenuItem(
+          children: AppMenuModel.menuList.asMap().entries.map((entry) {
+            final index = entry.key;
+            final e = entry.value;
+            return AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return FadeTransition(
+                  opacity: Tween<double>(
+                    begin: 0,
+                    end: 1,
+                  ).animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut)),
+                  child: SlideTransition(
+                    position:
+                        Tween<Offset>(
+                          begin: const Offset(-0.5, 0),
+                          end: Offset.zero,
+                        ).animate(
+                          CurvedAnimation(parent: animation, curve: Curves.easeOutQuart),
+                        ),
+                    child: child,
+                  ),
+                );
+              },
+              child: Padding(
+                key: ValueKey<int>(index),
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: MenuItem(
                   onTap: () {
                     scrollCubit.scrollToSection(e.path);
-                    if (!isFromFooter) {
-                      drawerCubit.changeDrawerState(false); // Close drawer
-                    }
+                    context.read<DrawerMenuCubit>().changeDrawerState(false);
                   },
                   isSelected: e.path == scrollCubit.currentsctionPath,
                   title: e.title,
                 ),
-              )
-              .toList(),
+              ),
+            );
+          }).toList(),
         );
       },
     );
